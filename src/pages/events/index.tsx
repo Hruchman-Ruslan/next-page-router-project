a25ecb68cef4502d30c96f3c3011cdb35e86457d
Ruslan <Giovingo92@gmail.com>
2024-06-18 13:49:37 +0300

    feat: filter form to filtering events

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,9 +1,19 @@
+import { Fragment } from "react";
+
+import { getAllEvents } from "../../../dummy-data";
+
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
+
 export interface AllEventsPageProps {}
 
 export default function AllEventsPage({}: AllEventsPageProps) {
+  const events = getAllEvents();
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
   );
 }
